b96f146dba2d2a8568cb75c30fd660a246e4836f
App Academy Student <brentalansmith@gmail.com>
2014-03-19 12:04:44 -0700

    change app.js

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(:default, Rails.env)
 
 module Docket
   class Application < Rails::Application
-    config.assets.enabled = false
     config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
