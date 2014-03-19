b8b5e0df31c089a6c622836cc899b0e1b47e305a
App Academy Student <brentalansmith@gmail.com>
2014-03-19 11:21:34 -0700

    initial push to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140319172415) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "calendars", force: true do |t|
     t.string   "title"
     t.integer  "user_id"
