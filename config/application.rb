0934625dc71bb0a7ff8dc05641a581431a827926
App Academy Student <brentalansmith@gmail.com>
2014-03-19 11:54:58 -0700

    updated image path for precompile

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(:default, Rails.env)
 module Docket
   class Application < Rails::Application
     config.assets.enabled = false
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
