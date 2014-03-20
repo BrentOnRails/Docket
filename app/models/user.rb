fc2d47b6c297df413b19efec887332981eca6877
App Academy Student <brentalansmith@gmail.com>
2014-03-20 15:22:36 -0700

    played with OAuth-Devise and figured out how to rename the crappy provider name, whee

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,7 @@ class User < ActiveRecord::Base
 
    has_many :calendars
    has_many :events, :through => :calendars, :source => :events
+   has_many :authorizations
 
 
    def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -229,7 +229,9 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :google_oauth2, ENV["GOOGLE_API_KEY"], ENV["GOOGLE_SECRET"]
+  config.omniauth :google_oauth2, ENV["GOOGLE_API_KEY"], ENV["GOOGLE_SECRET"], {
+    name: "google"
+  }
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
