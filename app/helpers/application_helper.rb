79461877c4b31be97fbe2b273574bf30a94ca132
App Academy Student <brentalansmith@gmail.com>
2014-03-21 10:18:42 -0700

    updated image size

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,14 @@ module ApplicationHelper
     if user.avatar_url.present?
       user.avatar_url
     else
-      default_url = "#{root_url}images/guest.png"
+
       gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-      "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
+
+      "http://gravatar.com/avatar/#{gravatar_id}.png"
     end
   end
 
 end
+
+# default_url = "#{root_url}images/guest.png"
+# http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
