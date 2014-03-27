24e43db04e8c0888041aaf49cc256f2761855206
App Academy Student <brentalansmith@gmail.com>
2014-03-26 17:41:52 -0700

    rollback

--- Gemfile
+++ Gemfile
@@ -42,5 +42,3 @@ gem 'figaro'
 gem 'omniauth-google-oauth2'
 gem 'uuidtools'
 
-#async mailer
-gem "devise-async"
--- app/models/user.rb
+++ app/models/user.rb
@@ -23,8 +23,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable,
-         :async
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
    has_many :calendars, dependent: :destroy
    has_many :events, :through => :calendars, :source => :events
