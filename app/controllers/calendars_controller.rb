894c97281a4f36142a4ab72b574a31585fdfd90e
Brent Smith <Brentalansmith@gmail.com>
2014-04-10 01:23:17 -0700

    updated data that populates for demo user

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -13,13 +13,13 @@ class CalendarsController < ApplicationController
   def demo_data
     cal = current_user.calendars.build(title: "test calendar", user_id: current_user.id)
     cal.save
-    cal.events.build(title: "Plan Example", notes: "Entries with no date are 'Plan'").save
-    cal.events.build(title: "Plan Example", notes: "Entries with no date are 'Plan'").save
-    cal.events.build(title: "Plan Example", notes: "Entries with no date are 'Plan'").save
-    cal.events.build(title: "Plan Example", notes: "Entries with no date are 'Plan'").save
-    cal.events.build(title: "Plan Example", notes: "Entries with no date are 'Plan'").save
-    cal.events.build(title: "Plan Example", notes: "Entries with no date are 'Plan'").save
-    cal.events.build(title: "Plan Example", notes: "Entries with no date are 'Plan'").save
+    cal.events.build(title: "Plan Example", notes: "Entries with no date are 'Plans'").save
+    cal.events.build(title: "Today Example", date: 1.second.ago, notes: "The Today Tab shows events for today!'").save
+    cal.events.build(title: "Week Example", date: 5.days.from_now, notes: "Anything 7 days or less in the future shows on the week tab").save
+    cal.events.build(title: "Month Example", date: 20.days.from_now, notes: "Anything 30 days or less in the future shows on month tab").save
+    cal.events.build(title: "Visit my portfolio site", notes: "BrentOnRails.com").save
+    cal.events.build(title: "Hite a Web Dev!", notes: "I'm one and I know others, hit me up!").save
+    cal.events.build(title: "Check Back!", date: 1.day.from.now, notes: "Theres new features added everyday!").save
     return current_user.calendars
   end
 
